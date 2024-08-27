c98ba740d00d3bc37b351bd368ea9acb69cf8498
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2024-08-28 00:07:10 +0700

    change file's name

---
+++ API/Configuration/HealthChecksConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace API.Configuration
+{
+    public static class HealthChecksConfiguration
+    {
+        public static IServiceCollection ConfigureHealthChecks(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var hcBuilder = services.AddHealthChecks();
+
+            return services;
+        }
+
+        public static IEndpointRouteBuilder MapDefaultHealthChecks(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapHealthChecks("/health_check", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            return endpoints;
+        }
+    }
+}
