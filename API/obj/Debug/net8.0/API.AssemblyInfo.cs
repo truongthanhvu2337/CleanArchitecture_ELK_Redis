42f9f8c90c2ce32c4736c114fae10ed6ee6d074a
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2024-08-31 16:40:20 +0700

    test

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70b6f5a729d2106fcb7c8e8630d3d7bc1d988a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00493dbde2c974f9b87c462045d7839bf31122cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
